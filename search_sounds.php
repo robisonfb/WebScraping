25d6c9a64c26fbc5f48f6af3f1626adc077ffc0a
robison F. Borges <robisonfb@outlook.com>
2021-10-20 09:54:47 -0300

    run sound

--- search_sounds.php
+++ search_sounds.php
@@ -5,27 +5,26 @@ function get_http_response_code($url) {
 }
 
 function save_file($namefile, $content){
-    $file_sounds = fopen("sounds/" . $namefile , "w") or die("Unable to open file!");
+    $file_sounds = fopen("sounds/" . $namefile , "a") or die("Unable to open file!");
     fwrite($file_sounds, $content);
     fclose($file_sounds);
 }
 
 $url = "https://www.cifras.com.br/arquivos/sound/guitar/";
 
-
 for ($i = 0; $i <= 10; $i++) {
     $url_name_segment_1 = $i . "_";
-    for ($x = 0; $x <= 10; $x++) {
+    for ($x = 0; $x <= 50; $x++) {
 
         $url_segment = $url_name_segment_1 . $x . '.txt';
 
         if(get_http_response_code($url . $url_segment) != "200"){
-            save_file("errors.txt", $url_segment);
+            save_file("errors.txt", $url_segment."\r\n");
         }else{
             $content = file_get_contents($url . $url_segment);
             save_file($url_segment, $content);
         }
-
+        echo "running... \r\n";
     }
 }
 
